1a7496d4f0fb4287edd32b9d6ab1f86c74e49ce2
Ham seong woo <a01098679934@gmail.com>
2026-04-06 20:01:16 +0900

    Add Input Component

---
+++ src/components/auth/Input.jsx
@@ -0,0 +1,14 @@
+export function Input({ placeholder }) {
+  return (
+    <input
+      placeholder={placeholder}
+      className="
+        w-96 bg-gray-50 h-card rounded-md 
+        border-2 border-gray-300 
+        px-4 py-2 text-sm font-medium text-black 
+        focus:outline-none 
+        focus:border-P400
+      "
+    />
+  );
+}
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,6 +1,14 @@
 import { Button } from "../../components/auth/Button";
+import { Input } from "../../components/auth/Input";
 
-export function Login(){
-    return <Button>로그인</Button>;
-    
+export function Login() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen gap-6">
+        <Input placeholder="이메일" />
+        <Input placeholder="비밀번호" />
+        <Button>로그인</Button>
+      </div>
+    </>
+  );
 }
