9e933e85880aa8dfce9150cc52abec9ff9a9dcd2
Ham seong woo <a01098679934@gmail.com>
2026-04-06 18:13:12 +0900

    Add Button Component

---
+++ src/pages/auth/Login.jsx
@@ -0,0 +1,6 @@
+import { Button } from "../../components/auth/Button";
+
+export function Login(){
+    return <Button>로그인</Button>;
+    
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        P400: "#F5AFAF",
+      },
+      width: {
+        card: "335px",
+      },
+      height:{
+        card: "60px",
+      }
+    },
+  },
   plugins: [],
-}
+};
