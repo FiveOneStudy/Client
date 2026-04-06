7445df3a20668f31342c70c2f7bac14cc316666f
Ham seong woo <a01098679934@gmail.com>
2026-04-06 20:37:25 +0900

    Fix Button Component

--- src/components/auth/Button.jsx
+++ src/components/auth/Button.jsx
@@ -1,6 +1,6 @@
 export function Button({children}){
     return (
-        <button className="bg-P400 text-white w-card h-card rounded-md font-black text-xl text-center ">
+        <button className="bg-P400 text-white w-card h-card rounded-md font-black text-lg text-center ">
             {children}
         </button>
     );
