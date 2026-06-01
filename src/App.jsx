1f685c663d16f05122fced6800972248543a78dd
Ham seong woo <a01098679934@gmail.com>
2026-06-01 09:21:53 +0900

    fix: auth ui 개선

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,54 @@
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Nav } from './components/Nav.jsx';
+
 import { Login } from './pages/auth/Login.jsx';
 import { SignUp } from './pages/auth/SignUp.jsx';
 import { FindPassword } from './pages/auth/FindPassword.jsx';
 import { PasswordReset } from './pages/auth/PasswordReset.jsx';
+
 import { My } from './pages/My.jsx';
-import { Plan } from "./pages/Plan.jsx";
+import { Plan } from './pages/Plan.jsx';
+
 import { Community } from './pages/community/Community.jsx';
 import { Popularity } from './pages/community/Popularity.jsx';
 import { Recent } from './pages/community/Recent.jsx';
 import { Mypost } from './pages/community/Mypost.jsx';
 import { Mycomment } from './pages/community/Mycomment.jsx';
-import { Write } from './pages/community/Write.jsx'
+import { Write } from './pages/community/Write.jsx';
+
+// Nav가 필요한 페이지용 레이아웃
+function MainLayout() {
+  return (
+    <>
+      <Nav />
+      <Outlet />
+    </>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-
       <Routes>
+        {/* Nav 없이 표시될 페이지 */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/findpassword" element={<FindPassword />} />
         <Route path="/passwordreset" element={<PasswordReset />} />
-      </Routes>
-      <Nav />
-      <Routes>
-        <Route path="/my" element={<My />} />
-        <Route path="/plan" element={<Plan />} />
-        <Route path='/community' element={<Community/>} />
-        <Route path='/community/popularity' element={<Popularity/>} />
-        <Route path='/community/recent' element={<Recent/>} />
-        <Route path='/community/mypost' element={<Mypost/>} />
-        <Route path='/community/mycomment' element={<Mycomment/>} />
-        <Route path='/community/write' element={<Write/>} />
+
+        {/* Nav가 표시될 페이지 */}
+        <Route element={<MainLayout />}>
+          <Route path="/my" element={<My />} />
+          <Route path="/plan" element={<Plan />} />
+
+          <Route path="/community" element={<Community />} />
+          <Route path="/community/popularity" element={<Popularity />} />
+          <Route path="/community/recent" element={<Recent />} />
+          <Route path="/community/mypost" element={<Mypost />} />
+          <Route path="/community/mycomment" element={<Mycomment />} />
+          <Route path="/community/write" element={<Write />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
