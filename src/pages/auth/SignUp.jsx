747ce960a3852e79b11acec249a88a8a95f62007
Ham seong woo <a01098679934@gmail.com>
2026-06-04 16:55:26 +0900

    fix: password placeholder글씨 안보임 버그 수정

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -7,7 +7,6 @@ export function SignUp() {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="flex flex-col w-96 h-[500px]">
-
         {/* 위 영역 */}
         <div>
           <h1 className="text-2xl font-bold text-center mb-16 tracking-tight font-pretendard">
@@ -16,17 +15,15 @@ export function SignUp() {
 
           <div className="flex flex-col gap-4 w-full">
             <Input placeholder="이메일" />
-            <Password>비밀번호</Password>
-            <Password>비밀번호 재확인</Password>
+            <Password placeholder="비밀번호" />
+            <Password placeholder="비밀번호 재확인" />
           </div>
         </div>
 
         {/* 아래 영역 */}
         <div className="flex flex-col w-full mt-16 items-center">
           <Button className="w-full mb-4">회원가입</Button>
-
         </div>
-
       </div>
     </div>
   );
