43bf7116e95433aa85a807b24e918c5e928dea0f
xy-jxn <s24016@gsm.hs.kr>
2026-06-08 17:40:03 +0900

    feat : api 연결, post 구분 추가

--- src/components/community/PostListItem.jsx
+++ src/components/community/PostListItem.jsx
@@ -1,14 +1,19 @@
-export function PostListItem({ title, date, views }) {
+export function PostListItem({ title, createdAt, viewCount, onClick }) {
   return(
-    <div className="inline-flex h-[32px] px-7 justify-between items-center border-t border-[#B4B5B7]">
-      <div className="w-[720px] h-full flex items-center  bg-white">
+    <div
+      onClick={onClick}
+      className="inline-flex h-[32px] px-7 justify-between items-center border-b border-[#B4B5B7] cursor-pointer"
+    >
+      <div className="w-[720px] h-full flex items-center bg-white">
         <span className="text-black text-[14px] font-normal">{title}</span>
       </div>
+
       <div className="w-[160px] h-full flex items-center justify-center bg-white">
-        <span className="text-black text-[14px] font-normal">{date}</span>
+        <span className="text-black text-[14px] font-normal">{createdAt}</span>
       </div>
+
       <div className="w-[104px] h-full flex items-center justify-center bg-white">
-        <span className="text-black text-[14px] font-normal">{views}</span>
+        <span className="text-black text-[14px] font-normal">{viewCount}</span>
       </div>
     </div>
   );
--- src/components/community/WriteUser.jsx
+++ src/components/community/WriteUser.jsx
@@ -1,14 +1,16 @@
 import profile from '../../assets/profile.svg';
 
-export function WriteUser({ name, date, views }) {
+export function WriteUser({ writer, createdAt, viewCount }) {
   return(
     <div className='flex flex-row gap-2 mb-[12px] py-[8px] border-b border-G300'>
       <img src={profile} className="w-[40px]" />
       <div>
-        <div className='text-[16px]'>{name}</div>
+        <div className='text-[16px]'>{writer}</div>
         <div className='flex flex-row gap-2'>
-          <div className='text-[12px] text-G500 font-light'>{date}</div>
-          <div className='text-[12px] text-G500 font-light'>조회 {views}</div>
+          <div className='text-[12px] text-G500 font-light'>{createdAt}</div>
+          {viewCount === 'write' ? <div></div> : 
+            <div className='text-[12px] text-G500 font-light'>조회 {viewCount}</div>
+          }
         </div>
       </div>
     </div>
