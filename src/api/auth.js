c87792dfe34fc06e80b606b0f1aad44503b75707
Ham seong woo <a01098679934@gmail.com>
2026-06-09 17:00:56 +0900

    feat: 비밀번호 재설정 api 연결

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { BASE_URL } from "./index";
+import { BASE_URL, getToken } from "./index";
 
 export const login = async (email, password) => {
   return axios.post(`${BASE_URL}/login`, { email, password });
@@ -7,4 +7,28 @@ export const login = async (email, password) => {
 
 export const signup = async (email, password, nickname) => {
   return axios.post(`${BASE_URL}/signup`, { email, password, nickname });
+};
+
+export const sendAuthCode = async (email) => {
+  return axios.post(
+    `${BASE_URL}/password/reset/code`,
+    { email },
+    {
+      headers: {
+        Authorization: `Bearer ${getToken()}`,
+      },
+    }
+  );
+};
+
+export const resetPassword = async (email, code, newPassword) => {
+  return axios.patch(
+    `${BASE_URL}/password/reset`,
+    { email, code, newPassword },
+    {
+      headers: {
+        Authorization: `Bearer ${getToken()}`,
+      },
+    }
+  );
 };
