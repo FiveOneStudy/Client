8ed2a9c7bac6fceaca700cb0c895245563f324c4
Ham seong woo <a01098679934@gmail.com>
2026-06-09 15:55:09 +0900

    fix: accesstoken 저장

--- src/api/index.js
+++ src/api/index.js
@@ -2,4 +2,4 @@ export const BASE_URL = 'https://port-0-server-m1ed5avw1d3364c3.sel4.cloudtype.a
 
 const TOKEN = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTc4MDkwOTUxNSwiZW1haWwiOiJzMjQwMjZAZ3NtLmhzLmtyIn0.ebZAKNSt31mTffoZ_MxyLPXa0JAmYEZmoBXcIh5NWL6BHHio4GV2ZmtSd54fIbhFjU4XNKm2PJhU6VVLAEZXPQ';
 
-export const getToken = () => localStorage.getItem('token') || TOKEN;
+export const getToken = () => localStorage.getItem('accessToken'); // 'token' → 'accessToken'
